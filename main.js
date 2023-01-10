41836d51bb3417eeb42acd1ef75a14a1a4be340b
Jessa Challa <jessa.challa@gmail.com>
2023-01-10 10:05:47 -0500

    cleaning up, style changes, adds title and description

--- main.js
+++ main.js
@@ -19,50 +19,15 @@ let valentineSchmitty = {
 
 let valentineArray = [valentineRudy, valentinePom, valentineSchmitty];
 
-
-
-// // Set up function setHTMLValentinesName here
-// function setHTMLValentinesName(valentine){
-//     let valentineHTML = document.getElementById(`${valentine.id}--name`);
-//     valentineHTML.innerHTML = valentine.name;
-// }
-// // setHTMLValentinesName(valentineRudy);
-
-
-// // Set up function setHTMLValentinesMessage here
-// function setHTMLValentinesMessage(valentine){
-//     let valentineHTML = document.getElementById(`${valentine.id}--message`);
-//     valentineHTML.innerHTML = valentine.message;
-// }
-// // setHTMLValentinesMessage(valentineRudy);
-
-
-// // Set up function processValentines here
-// function processValentines(){
-//     let n = 0;
-//     while (n<valentineArray.length) {
-//     let valentine = valentineArray[n];
-//     setHTMLValentinesName(valentine);
-//     setHTMLValentinesMessage(valentine);
-//     n++;
-//     }
-// }
-
-
-// // Call process Valentines here
-// processValentines()
-
-
-
-function allIn1(){
+function processValentines() {
     let n = 0;
-    let i = n+1;
-    while (n<valentineArray.length) {
-    document.getElementById(`valentine-${i}--name`).innerHTML = (`${valentineArray[n].name}`);
-    document.getElementById(`valentine-${i}--message`).innerHTML = (`${valentineArray[n].message}`);
-    n++;
-    i++;
+    let i = n + 1;
+    while (n < valentineArray.length) {
+        document.getElementById(`valentine-${i}--name`).innerHTML = (`${valentineArray[n].name}`);
+        document.getElementById(`valentine-${i}--message`).innerHTML = (`${valentineArray[n].message}`);
+        n++;
+        i++;
     }
- }
+}
 
- allIn1()
+processValentines()
